be2aa5e2a63616233dd2cac79714bb96e1b7689c
sjm <sujinming0125@163.com>
2016-09-23 19:24:25 +0800

    Change Two Pages
    404: change style
    contest_end: change style

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,44 +3,61 @@
 <head>
     <title>404</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
     <style>
-        html, body {
-            height: 100%;
-        }
 
         body {
-            margin: 0;
-            padding: 0;
             width: 100%;
-            color: #B0BEC5;
-            display: table;
+            color: #b7bdc0;
             font-weight: 100;
-            font-family: 'Lato';
+            background-color: #f3f3f3;
         }
-
-        .container {
+        .header {
+            margin-top: 15%;
             text-align: center;
-            display: table-cell;
-            vertical-align: middle;
+        }
+        .my_404 {
+            display: block;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 65px;
+        }
+        .content{
+            margin-top: 5%;
         }
 
-        .content {
-            text-align: center;
-            display: inline-block;
+        .my_a{
+            font-family: Arial, Helvetica, sans-serif;
+            color: #5cb85c;
+            opacity: 0.8;
+            text-decoration: none;
+            font-size:15px;
         }
-
-        .title {
-            font-size: 72px;
-            margin-bottom: 40px;
+        .my_a:hover {
+            color: #5cb85c;
+            opacity: 0.9;
+            font-size: 17px;
+        }
+        .hint{
+            font-family: Arial, Helvetica, sans-serif;
+            margin-top: 3%;
+            margin-left: 37%;
+            line-height: 25px;
+            font-size: 16px;
         }
     </style>
 </head>
 <body>
-<div class="container">
-    <div class="content">
-        <img src="/image/404.png" height="427" width="640"><br />
+<div class="header">
+    <div class="my_404"><em>404 ERROR</em></div>
+</div>
+<div class="content">
+    <a href="/" class="my_a" style="float: left; margin-left: 38%"><strong>Back to Home Page</strong></a>
+    <a href="/" class="my_a" style="float: right; margin-right: 38%" onclick="history.go(-1)"><strong>Back to Last Page</strong></a>
+    <div style="clear: both"></div>
+    <div class="hint">
+        Oops! Page Not Found <br/>
+        If you are tired, you can read some interesting news on <a href="http://www.neu.edu.cn" class="my_a" style="color: indianred;"><strong>NEU website</strong></a>.<br/>
+        Also, you can <a href="http://oj.neu.edu.cn" class="my_a" style="color: indianred;"><strong>go on practicing</strong></a>.<br/>
+        Good Luck!<br/>
     </div>
 </div>
 </body>
--- resources/views/errors/contest_end.blade.php
+++ resources/views/errors/contest_end.blade.php
@@ -4,20 +4,37 @@
     <title>Contest Ended!</title>
     @include("layout.head")
     <link rel="stylesheet" href="/css/main.css">
+    <style>
+        body {
+            background-color: #f3f3f3;
+        }
+        .my_404 {
+            display: block;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 35px;
+        }
+        .content {
+            margin-top: 5%;
+        }
+        .content img{
+            opacity: 0.7;
+        }
+        .content span {
+            font-family:"YaHe Arial", Arial, Helvetica, sans-serif;
+            letter-spacing: 2px;
+        }
+    </style>
 </head>
 <body>
-    @include("layout.header")
-    <h1 class="text-center">QWQ</h1>
-    <h1 class="text-center">Contest Ended!</h1>
+@include("layout.header")
 
-    <div align="center"><img src="/image/cry.jpg" height="50%"/></div>
-
-    <div class="text-center">
-        You are not allowed to submit now<br/>
-        Click <a href="/contest/{{ $contest->contest_id }}">Here</a> to back to contest Page
-    </div>
-    <br/>
-    <br/>
-    @include("layout.footer")
+<div class="text-center content">
+    <div class="my_404 text-center">Contest Ended!</div><br/><br/>
+    <img src="/image/contest_end.gif"/><br/><br/><br/>
+    <span class="text-muted">You are not allowed to submit now, Click <a href="/contest/{{ $contest->contest_id }}"><b>Here</b></a> to back to contest Page</span>
+</div>
+<br/>
+<br/>
+@include("layout.footer")
 </body>
 </html>
